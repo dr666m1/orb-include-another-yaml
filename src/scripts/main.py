ab0d6299b849261ab7eb42c1927f4fe6bae3f880
dr666m1 <skndr666m1@gmail.com>
2022-12-25 13:43:24 +0900

    update python scripts

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,6 +1,58 @@
-def temp():
-    return "dummy"
+import contextlib
+import os
+import yaml
+
+
+class Import(yaml.YAMLObject):
+    yaml_tag = "!include"
+
+    @classmethod
+    def from_yaml(cls, _, node):
+        filepath = None
+        keypath = None
+
+        if isinstance(node, yaml.ScalarNode):
+            filepath = node.value
+        else:
+            for item in node.value:
+                match key := item[0].value:
+                    case "filepath":
+                        filepath = item[1].value
+                    case "keypath":
+                        keypath = item[1].value
+                    case _:
+                        raise Exception(f"got unnexpected parameter: {key}")
+
+        if filepath is None:
+            raise Exception("filepath is not specified!")
+
+        with open(filepath, "r") as f:
+            obj = yaml.full_load(f)
+
+        if keypath is not None:
+            obj = obj[keypath]
+        return obj
+
+
+def main(filepath=None, overwrite=True):
+    if filepath == None:
+        filepath = os.getenv("PARAM_FILEPATH")
+
+    if filepath is None:
+        raise Exception("PARAM_FILEPATH is not specified!")
+
+    with (
+        open(filepath, "r") as f,
+        contextlib.chdir(os.path.dirname(filepath)),
+    ):
+        obj = yaml.full_load(f)
+
+    if overwrite:
+        with open(filepath, "w") as f:
+            f.write(yaml.dump(obj))
+
+    return obj
 
 
 if __name__ == "__main__":
-    print(temp())
+    main()
--- src/scripts/test.py
+++ src/scripts/test.py
@@ -1,11 +1,28 @@
+import os
+import pathlib
 import unittest
+import yaml
 
 import main
 
+TESTCASE_PATH = pathlib.Path("../../testcase")
+
 
 class TestMain(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual(main.temp().upper(), "DUMMY")
+    def test_all(self):
+        input_paths = os.listdir(TESTCASE_PATH / "input")
+        input_paths = filter(
+            lambda x: os.path.isfile(TESTCASE_PATH / "input" / x), input_paths
+        )
+
+        for input_path in input_paths:
+            with self.subTest(msg=input_path):  # msg is displayed when a subtest fails
+                actual = main.main(TESTCASE_PATH / "input" / input_path, False)
+                with open(
+                    TESTCASE_PATH / "expected" / input_path, "r"
+                ) as expected_file:
+                    expected = yaml.full_load(expected_file)
+                self.assertEqual(actual, expected)
 
 
 if __name__ == "__main__":
